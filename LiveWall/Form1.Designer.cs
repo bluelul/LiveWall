e9eadb5dce3b1205cfe1ea0abad18360d1816b20
lttung1197 <lttung1197bt@gmail.com>
2020-01-22 02:30:57 +0700

    Add files via upload

---
+++ LiveWall/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace LiveWall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnChooseWall = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblNamedetail = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.cbxStartup = new System.Windows.Forms.CheckBox();
+            this.lblfilename = new System.Windows.Forms.Label();
+            this.lblsyntaxTitle = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnChooseWall
+            // 
+            this.btnChooseWall.Location = new System.Drawing.Point(10, 11);
+            this.btnChooseWall.Name = "btnChooseWall";
+            this.btnChooseWall.Size = new System.Drawing.Size(122, 103);
+            this.btnChooseWall.TabIndex = 0;
+            this.btnChooseWall.Text = "Choose\r\nWallpapers";
+            this.btnChooseWall.UseVisualStyleBackColor = true;
+            this.btnChooseWall.Click += new System.EventHandler(this.btnChooseWall_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "LiveWall";
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblNamedetail
+            // 
+            this.lblNamedetail.AutoSize = true;
+            this.lblNamedetail.Location = new System.Drawing.Point(141, 49);
+            this.lblNamedetail.Name = "lblNamedetail";
+            this.lblNamedetail.Size = new System.Drawing.Size(103, 39);
+            this.lblNamedetail.TabIndex = 6;
+            this.lblNamedetail.Text = "hh: hour [0:23] \r\nD: day of week [2:8]\r\next: png, jpg, bmp,...";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(259, 146);
+            this.tabControl1.TabIndex = 8;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.Transparent;
+            this.tabPage1.Controls.Add(this.cbxStartup);
+            this.tabPage1.Controls.Add(this.lblfilename);
+            this.tabPage1.Controls.Add(this.lblsyntaxTitle);
+            this.tabPage1.Controls.Add(this.btnChooseWall);
+            this.tabPage1.Controls.Add(this.lblNamedetail);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(251, 120);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Main";
+            // 
+            // cbxStartup
+            // 
+            this.cbxStartup.AutoSize = true;
+            this.cbxStartup.Checked = true;
+            this.cbxStartup.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbxStartup.Location = new System.Drawing.Point(141, 97);
+            this.cbxStartup.Name = "cbxStartup";
+            this.cbxStartup.Size = new System.Drawing.Size(96, 17);
+            this.cbxStartup.TabIndex = 9;
+            this.cbxStartup.Text = "Run at start up";
+            this.cbxStartup.UseVisualStyleBackColor = true;
+            this.cbxStartup.CheckedChanged += new System.EventHandler(this.cbxStartup_CheckedChanged);
+            // 
+            // lblfilename
+            // 
+            this.lblfilename.AutoSize = true;
+            this.lblfilename.Location = new System.Drawing.Point(141, 30);
+            this.lblfilename.Name = "lblfilename";
+            this.lblfilename.Size = new System.Drawing.Size(88, 13);
+            this.lblfilename.TabIndex = 8;
+            this.lblfilename.Text = "hhD.ext or hh.ext";
+            // 
+            // lblsyntaxTitle
+            // 
+            this.lblsyntaxTitle.AutoSize = true;
+            this.lblsyntaxTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblsyntaxTitle.Location = new System.Drawing.Point(139, 13);
+            this.lblsyntaxTitle.Name = "lblsyntaxTitle";
+            this.lblsyntaxTitle.Size = new System.Drawing.Size(87, 13);
+            this.lblsyntaxTitle.TabIndex = 7;
+            this.lblsyntaxTitle.Text = "Image Syntax:";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(251, 120);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "About";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(240, 91);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Change wallpaper following time and day of week\r\n\r\nVersion: 1.0\r\n\r\nAuthor: lttung" +
+                "1197\r\n\r\nRelease date: 01/2020\r\n";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(105, 26);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(104, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(256, 146);
+            this.Controls.Add(this.tabControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LiveWall";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChooseWall;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblNamedetail;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label lblfilename;
+        private System.Windows.Forms.Label lblsyntaxTitle;
+        private System.Windows.Forms.CheckBox cbxStartup;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+    }
+}
+
---
+++ LiveWall/Form1.cs
@@ -0,0 +1,456 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+
+namespace LiveWall
+{
+    public partial class Form1 : Form
+    {
+        //=============================================
+        // Define
+        const string softwareName = "LiveWall";
+
+        //=============================================
+        // Init Param
+        System.DayOfWeek dowNow = DateTime.Now.DayOfWeek;
+        int hourNow = DateTime.Now.Hour;
+
+        //=============================================
+        // Global Variables
+        string wallDirStr = Properties.Settings.Default["WallDirStr"].ToString();
+
+        List<int> hourList = new List<int>();
+        List<int> dowList = new List<int>();
+        string[] wallList = new string[0];
+
+        int lastHour = -1;
+        int lastDow = -1;
+        int lastIndexMatch = -1;
+
+        int maxHourIndex = 0;
+        int minHourIndex = 0;
+
+        //=============================================
+        // Init Process
+        public Form1()
+        {
+            InitializeComponent();
+
+            //-------------------------------
+            // Check duplicate running application
+            Process[] processlist = Process.GetProcesses();
+            int count = 0;
+            foreach (Process theprocess in processlist)
+            {
+                string processDesc;
+                try
+                {
+                    processDesc = FileVersionInfo.GetVersionInfo(theprocess.MainModule.FileName).FileDescription;
+                }
+                catch
+                {
+                    processDesc = String.Empty;
+                }
+                if (processDesc == "LiveWall")
+                    count++;
+            }
+            
+            if (count > 1)
+            {
+                MessageBox.Show("Another LiveWall process is running","Error");
+
+                notifyIcon1.Visible = false;
+                System.Environment.Exit(1); 
+            }
+              
+            //-------------------------------
+            // load "Run at start up" state
+            Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            if (key.GetValue(softwareName) != null)
+            {
+                if (key.GetValue(softwareName).ToString() == Application.ExecutablePath)
+                    cbxStartup.Checked = true;
+                else
+                    cbxStartup.Checked = false;
+            }
+            else
+                cbxStartup.Checked = false;
+
+            //-------------------------------
+            // notify icon menu
+            notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+            
+            //-------------------------------
+            // load Wall Dir & Wall List
+            string wallListStr = Properties.Settings.Default["WallListStr"].ToString();
+            if ((wallDirStr != String.Empty) && (wallListStr != String.Empty))
+            {
+                // load wall list
+                string[] wallListTemp = wallListStr.Split('\n');
+
+                // check valid path files
+                List<int> failIndex = new List<int>();
+                for (int i = 0; i < wallListTemp.Length; i++)
+                    if (File.Exists(Path.Combine(wallDirStr, wallListTemp[i])) == false)
+                        failIndex.Add(i);
+                
+                // valid path
+                if (failIndex.Count == 0)
+                {
+                    // process and save valid data
+                    processValidData(wallListTemp);
+
+                    // update wallpaper
+                    UpdateWall();
+                }
+                
+                // not valid path
+                else
+                {
+                    string errorString = String.Empty;
+                    for (int i = 0; i < failIndex.Count; i++)
+                    {
+                        errorString += Path.Combine(wallDirStr, wallListTemp[failIndex[i]]);
+                        errorString += "\n";
+                    }
+                    errorString += "does not exist.";
+
+                    wallDirStr = String.Empty;
+                    wallListStr = String.Empty;
+                    Properties.Settings.Default["WallDirStr"] = String.Empty;
+                    Properties.Settings.Default["WallListStr"] = String.Empty;
+                    Properties.Settings.Default.Save();
+                    
+                    MessageBox.Show(errorString, "Path Error");
+                }
+            }
+            
+            notifyIcon1.Visible = true;
+
+            //-------------------------------
+            // First Run
+            if (Properties.Settings.Default["FirstRun"].ToString() == "True")
+            {
+                this.WindowState = FormWindowState.Normal;
+
+                Properties.Settings.Default["FirstRun"] = false;
+                Properties.Settings.Default.Save();
+            }
+        }
+
+        //=============================================
+        // Import Wallpaper
+        private void btnChooseWall_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Multiselect = true;
+            openFileDialog1.ShowDialog();
+        }
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            string wallDirStrTemp = Path.GetDirectoryName(openFileDialog1.FileNames[0]);
+
+            string[] wallListTemp = new string[openFileDialog1.FileNames.Length];
+
+            // check valid names
+            List<int> failIndex = new List<int>();
+            
+            for (int i = 0; i < openFileDialog1.FileNames.Length; i++)
+            {
+                wallListTemp[i] = Path.GetFileName(openFileDialog1.FileNames[i]);
+
+                bool checkValid = false;
+                if (wallListTemp[i].Contains(".") == true)
+                {
+                    string nameWall = wallListTemp[i].Split('.')[0];
+                    if (nameWall.Length == 2)
+                    {
+                        Int16 nameInt;
+                        if (Int16.TryParse(nameWall, out nameInt))
+                        {
+                            if ((nameInt >= 0) && (nameInt <= 23))
+                                checkValid = true;
+                        }
+                                
+                    }
+                    else if (nameWall.Length == 3)
+                    {
+                        Int16 nameInt;
+                        if (Int16.TryParse(nameWall, out nameInt))
+                        {
+                            if ((nameInt / 10 >= 0) && (nameInt / 10 <= 23) &&
+                                (nameInt % 10 >= 2) && (nameInt % 10 <= 8))
+                                checkValid = true;
+                        }
+                    }
+                }
+
+                if (checkValid == false)
+                    failIndex.Add(i);
+            }
+
+            // valid path
+            if (failIndex.Count == 0)
+            {
+                // Save config
+                wallDirStr = wallDirStrTemp;
+
+                string wallListStr = String.Empty;
+                for (int i = 0; i < wallListTemp.Length; i++)
+                {
+                    if (i == 0) wallListStr += wallListTemp[i];
+                    else wallListStr += "\n" + wallListTemp[i];
+                }
+
+                Properties.Settings.Default["WallDirStr"] = wallDirStr;
+                Properties.Settings.Default["WallListStr"] = wallListStr;
+                Properties.Settings.Default.Save();
+
+
+                // process and save valid data
+                processValidData(wallListTemp);
+
+                // update wallpaper
+                UpdateWall();
+            }
+
+            // not valid path
+            else
+            {
+                string errorString = String.Empty;
+                for (int i = 0; i < failIndex.Count; i++)
+                {
+                    errorString += Path.Combine(wallDirStrTemp, wallListTemp[failIndex[i]]);
+                    errorString += "\n";
+                }
+                errorString += "does not match syntax.";
+
+                MessageBox.Show(errorString, "Syntax Error");
+            }
+        }
+        void processValidData(string[] wallListInput)
+        {
+            wallList = wallListInput;
+            
+            // Export file name to hour and dow
+            hourList.Clear();
+            dowList.Clear();
+            maxHourIndex = 0;
+            minHourIndex = 0;
+            
+            for (int i = 0; i < wallList.Length; i++)
+            {
+                string nameWall = wallList[i].Split('.')[0];
+                hourList.Add((nameWall[0] - '0') * 10 + (nameWall[1] - '0'));
+
+                if (nameWall.Length == 3) 
+                    if (nameWall[2] == '8')
+                        dowList.Add(0);
+                    else
+                        dowList.Add(nameWall[2] - '1');
+                else dowList.Add(7); // no need dow
+
+                if (i > 0)
+                {
+                    if (hourList[maxHourIndex] < hourList[i])
+                        maxHourIndex = i;
+                    if (hourList[minHourIndex] > hourList[i])
+                        minHourIndex = i;
+                }
+            }
+
+            // show result
+            string tooltipFileStr = wallDirStr;
+            for (int i = 0; i < wallList.Length; i++)
+                tooltipFileStr += "\n" + wallList[i];
+            toolTip1.SetToolTip(btnChooseWall, tooltipFileStr);
+            
+        }
+
+        //=============================================
+        // Timer Update
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            hourNow = DateTime.Now.Hour;
+            dowNow = DateTime.Now.DayOfWeek;
+
+            if ((hourNow != lastHour) || ((int)dowNow != lastDow))
+            {
+                UpdateWall();
+            }
+
+            freeMemory();
+        }
+        private void UpdateWall()
+        {
+            if ((wallDirStr != String.Empty) && (wallList.Length > 0))
+            {
+                hourNow = DateTime.Now.Hour;
+                dowNow = DateTime.Now.DayOfWeek;
+
+                // check import wallList match hour and dow 
+                int indexMatch = -1;
+                for (int i = 0; i < hourList.Count; i++)
+                {
+                    if ((hourNow >= hourList[minHourIndex]) &&
+                        (hourNow < hourList[maxHourIndex]))
+                    {
+                        if (hourNow >= hourList[i])
+                        {
+                            bool checkMatch = false;
+                            if (indexMatch == -1)
+                                checkMatch = true;
+                            else if (hourList[indexMatch] < hourList[i])
+                                checkMatch = true;
+                            if (checkMatch)
+                            {
+                                if (dowList[i] == (int)dowNow) indexMatch = i;
+                                else if (dowList[i] == 7) indexMatch = i;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        indexMatch = maxHourIndex;
+                    }
+                }
+
+                // new hour (and dow)
+                if ((indexMatch != -1) && (indexMatch != lastIndexMatch))
+                {
+                    lastHour = hourNow;
+                    lastDow = (int)dowNow;
+                    lastIndexMatch = indexMatch;
+
+                    Uri path = new Uri(Path.Combine(wallDirStr, wallList[indexMatch]));
+                    Wallpaper.Set(path, Wallpaper.Style.Fill);
+                }
+            }
+        }
+
+        //=============================================
+        // Run at start up
+        private void cbxStartup_CheckedChanged(object sender, EventArgs e)
+        {
+            Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+            if (cbxStartup.Checked == true)
+                key.SetValue(softwareName, Application.ExecutablePath);
+            else
+                key.DeleteValue(softwareName, false);
+        }
+
+        //=============================================
+        // Free Memory (reduce RAM usage)
+        private void freeMemory()
+        {
+            System.Diagnostics.Process.GetCurrentProcess().MinWorkingSet = System.Diagnostics.Process.GetCurrentProcess().MinWorkingSet;
+        }
+
+        //=============================================
+        // Notify icon
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                Show();
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Hide();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            Hide();
+        }
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false;
+            System.Environment.Exit(1);            
+        }
+    }
+}
+
+public sealed class Wallpaper
+{
+    Wallpaper() { }
+
+    const int SPI_SETDESKWALLPAPER = 20;
+    const int SPIF_UPDATEINIFILE = 0x01;
+    const int SPIF_SENDWININICHANGE = 0x02;
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+    public enum Style : int
+    {
+        Tile,
+        Center,
+        Stretch,
+        Fill,
+        Fit,
+        Span
+    }
+
+    public static void Set(Uri uri, Style style)
+    {
+        System.IO.Stream s = new System.Net.WebClient().OpenRead(uri.ToString());
+
+        System.Drawing.Image img = System.Drawing.Image.FromStream(s);
+        string tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
+        img.Save(tempPath, System.Drawing.Imaging.ImageFormat.Bmp);
+        
+        img.Dispose();
+        s.Dispose();
+
+        RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+        if (style == Style.Fill)
+        {
+            key.SetValue(@"WallpaperStyle", 10.ToString());
+            key.SetValue(@"TileWallpaper", 0.ToString());
+        }
+        if (style == Style.Fit)
+        {
+            key.SetValue(@"WallpaperStyle", 6.ToString());
+            key.SetValue(@"TileWallpaper", 0.ToString());
+        }
+        if (style == Style.Span) // Windows 8 or newer only!
+        {
+            key.SetValue(@"WallpaperStyle", 22.ToString());
+            key.SetValue(@"TileWallpaper", 0.ToString());
+        }
+        if (style == Style.Stretch)
+        {
+            key.SetValue(@"WallpaperStyle", 2.ToString());
+            key.SetValue(@"TileWallpaper", 0.ToString());
+        }
+        if (style == Style.Tile)
+        {
+            key.SetValue(@"WallpaperStyle", 0.ToString());
+            key.SetValue(@"TileWallpaper", 1.ToString());
+        }
+        if (style == Style.Center)
+        {
+            key.SetValue(@"WallpaperStyle", 0.ToString());
+            key.SetValue(@"TileWallpaper", 0.ToString());
+        }
+
+        SystemParametersInfo(SPI_SETDESKWALLPAPER,
+            0,
+            tempPath,
+            SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+    }
+
+}
